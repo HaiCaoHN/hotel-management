780c97f7255c3247d92749bea6746e9a9cc31691
HaiCaoHN <haict15112002@gmail.com>
2023-12-30 23:04:25 +0700

    secure

--- src/libs/constant.ts
+++ src/libs/constant.ts
@@ -4,7 +4,7 @@ enum Constant {
   LOCAL = "local",
   "ACCESS_TOKEN" = "access_token",
   "REFRESH_TOKEN" = "refresh_token",
-  GOOGLE_CLIENT_ID = "233839907762-tqpm201poaf9j59nbsj8n0tjgk30tduv.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID = "",
   GOOGLE_SECRET = "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",
   BACK_END_URL = "http://localhost:8080",
 }
